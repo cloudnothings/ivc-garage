cc82963cc28c4a65b9829ecb98f1e30bba7aa709
Carlos Rangel <carlos@rangel.us>
2022-10-26 00:56:51 -0700

    Baseline

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,14 @@ import type { Session } from "next-auth";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
+
       <Component {...pageProps} />
     </SessionProvider>
   );
